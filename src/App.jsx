8d7f81320d12175cb60fb9b54d6af1eb8d48ae65
KcastroT <a01798925@tec.mx>
2025-04-14 17:16:25 -0600

    Add: Recuperar contraseña screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import './App.css'
-import NavBar from './components/NavBar.jsx'
+import "./App.css";
+import NavBar from "./components/NavBar.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RecoverPassword from "./pages/RecoverPassword.jsx";
+
+/*
+
+Usando React Router se crean rutas para cada página de la aplicación, 
+en este caso la ruta principal ("/") sería la landing page de la aplicación.
+
+No todas las rutas serán visible desde App.js 
+Sin embargo las ponemos aquí, hasta que se complete el flujo de navegación
+
+Solo se coloca un BrowserRouter dentro de la aplicación
+Dentro de los componentes de react se colocan Links que permiten navegar entre rutas
+Ejemplo <Link to="/recover-password">Recuperar contraseña</Link>
+*/
 
 function App() {
-
   return (
-    <>
-      <h1 class="text-3xl font-bold underline text-center">
-        Hola mundo Trendify
-      </h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <h1 className="text-3xl font-bold underline text-center">
+              Hola mundo Trendify
+            </h1>
+          }
+        />
+        <Route path="/recover-password" element={<RecoverPassword />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
