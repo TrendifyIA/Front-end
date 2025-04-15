488bfd89d3e0fbe7a4cea9daaeaaa9af79b31a2b
Alexeimtz <105665026+Alexeimtz@users.noreply.github.com>
2025-04-14 22:48:59 -0600

    Se agregaron las pantallas del codigo de verificacion otp y codigo de equipo

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import './App.css'
 import NavBar from './components/NavBar.jsx'
+import { Routes, Route, Link } from 'react-router-dom';
+import CodigoEquipo from './components/CodigoEquipo.jsx';
+import VerificacionOTP from './components/VerificacionOTP.jsx';
+
 
 function App() {
-
   return (
-    <>
-      <h1 class="text-3xl font-bold underline text-center">
-        Hola mundo Trendify
-      </h1>
-    </>
-  )
+    <div>
+      <nav class="bg-blue-900 text-white p-4 flex gap-4">
+        <Link to="/codigo-equipo" class="hover:underline">Código de equipo</Link>
+        <Link to="/otp" class="hover:underline">Verificación OTP</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/codigo-equipo" element={<CodigoEquipo />} />
+        <Route path="/otp" element={<VerificacionOTP />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+// hice unas rutas rapidas para probar los componentes :3
