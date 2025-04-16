edfefd107b1a6e4c90bb538a0e9062d545b82b40
sandy-herrera-tec <a01798452@tec.mx>
2025-04-15 23:49:07 -0600

    Pantalla de Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import './App.css'
 import NavBar from './components/NavBar.jsx'
 import { Routes, Route, Link } from 'react-router-dom';
 import TerminosYCond from './components/TerminosYCond.jsx';
+import LandingPage from './components/LandingPage.jsx';
 
 function App() {
   return (
     <div>
       <nav class="bg-blue-900 text-white p-4 flex gap-4">
         <Link to="/terminos" class="hover:underline">Términos y Condiciones</Link>
+        <Link to="/inicio" class="hover:underline">Inicio</Link>
       </nav>
 
       <Routes>
         <Route path="/terminos" element={<TerminosYCond />} />
+        <Route path="/inicio" element={<LandingPage />} />
       </Routes>
     </div>
   );
