d94e300e148dbecd41829c31d1d79ff0eb9936f7
Andy12M <adocemur@gmail.com>
2025-04-22 20:09:52 -0600

    3 pantallas de encuestas e imagen del folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
 import './App.css'
-import NavBar from './components/NavBar.jsx'
+import NavBar from './components/NavBar.jsx' 
+import { useState } from 'react';
+import Encuestas from './components/Encuestas';
+import FolderEncuesta from './components/FolderEncuesta';
+
 
 function App() {
+  const [encuestas, setEncuestas] = useState([]);
+  const [encuestaActiva, setEncuestaActiva] = useState(null);
+
+  const agregarNuevaEncuesta = () => {
+    const nuevaEncuesta = {
+      id: Date.now(),
+      titulo: 'Título de encuesta',
+      ultimaModificacion: new Date().toLocaleDateString(),
+      preguntas: [],
+    };
+    setEncuestaActiva(nuevaEncuesta);
+  };
+
+  const guardarEncuesta = (encuestaGuardada) => {
+    setEncuestas((prev) => {
+      const actualizadas = prev.filter(e => e.id !== encuestaGuardada.id);
+      return [...actualizadas, encuestaGuardada];
+    });
+    setEncuestaActiva(null);
+  };
+
+  const editarEncuesta = (id) => {
+    const encuesta = encuestas.find(e => e.id === id);
+    setEncuestaActiva(encuesta);
+  };
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline text-center">
-        Hola mundo Trendify
-      </h1>
+      <NavBar/>
+      {encuestaActiva ? (
+        <FolderEncuesta
+          encuestaInicial={encuestaActiva}
+          onTerminarEncuesta={guardarEncuesta}
+        />
+      ) : (
+        <Encuestas
+          encuestas={encuestas}
+          onAgregarEncuesta={agregarNuevaEncuesta}
+          onEditarEncuesta={editarEncuesta}
+        />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
