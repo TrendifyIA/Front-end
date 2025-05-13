e9c34e419ee2fd353188d0a4d1fb8ae6df4e53fe
ERP604 <118367429+ERP604@users.noreply.github.com>
2025-05-13 14:02:01 -0600

    UPDATE registro funcional

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import CustomeButton from '../components/CustomButton.jsx';
+
 
 const Login = () => {
   const [credenciales, setCredenciales] = useState({
@@ -26,7 +28,14 @@ const Login = () => {
           <p className="mb-4">Bienvenido a KeySpotting.</p>
           <p className="mb-4">Inicia sesión para poder acceder a la mejor herramienta de tendencias en el mercado.</p>
           <p className="mb-6">¿Aún no tienes una cuenta? Regístrate dando clic en el siguiente botón.</p>
-          <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded">Registrarme</button>
+          <CustomeButton
+            texto="Registrarse"
+            onClick={() => console.log('Redirigiendo a registro...')}
+            tipo='terciario'
+            // Centrado del texto con tailwind
+            extraClases="text-center"
+            ruta="/simple/registro"
+          />
         </div>
 
         {/* Formulario */}
